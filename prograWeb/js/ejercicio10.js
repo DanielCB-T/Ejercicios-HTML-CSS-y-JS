2cfaeaf8e4a070f123d92edef5848a9ffdeaf82c
DanielCB-T <danielcruz.barrejo@gmail.com>
2026-07-03 00:03:51 -0600

    Delete prograWeb directory

--- prograWeb/js/ejercicio10.js
+++
@@ -1,6 +0,0 @@
-function convertir(){
-    let gradosC = document.getElementById("gradosC").value;
-    let fahrenheit = (parseInt(gradosC)*(9/5) + 32);
-
-    document.getElementById("fahrenheit").innerHTML = fahrenheit + ' °F';
-}
